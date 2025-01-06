a15c6f6e73ddfb722ca3a3e2de946fe760bfc6a0
hariyebk <harunbekri6@gmail.com>
2025-01-06 17:35:04 +0300

    gallery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,9 +1,20 @@
+import { GALLERY } from "@/constants";
+import Image from "next/image";
 
 
 export default function Gallery() {
     return (
-        <main className='min-h-screen'>
-            
+        <main className='min-h-screen max-md:my-20 md:my-32'>
+            <h2 className="max-sm:text-3xl sm:text-3xl lg:text-4xl text-black text-center font-medium "> Gallery </h2>
+            <div className="max-w-[1200px] mx-auto">
+                <div className="mt-7 flex flex-wrap items-start justify-center max-sm:px-6 gap-4">
+                    {GALLERY.map((image: string, index) => {
+                        return (
+                            <Image src={image} key={index} alt="staff-image" width={365} height={270} className="rounded-sm object-cover" />
+                        )
+                    })}
+                </div>
+            </div>
         </main>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen">
-
+      
     </main>
   );
 }
